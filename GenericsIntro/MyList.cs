ddbaea0c8c02b05936400fb53ed30bc79139d41e
Rumeysa Sena ÖZDEMİR <ozrumeysasena@gmail.com>
2023-09-19 20:25:57 +0300

    19 eylül ders sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    class MyList<T> // T-tip-type 
+    {
+        T[] items; //constructor
+        public MyList()
+        {
+                items = new T[0];
+        }
+        public void Add(T item)    //item-eleman , yerine başka bir şey yazsanda olur.
+        {
+            T[] tempArray = items; // geçici dizinin referansı itemsın referansı
+            items = new T[items.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length-1] = item;
+        } 
+    }
+}
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Rumeysa", "Sena", "Özdemir" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+
+            ////arrayler onu oluşturduğumuz sınırlarda takılırlar sadece. dışarıdan isimler[3]="asd"; şeklinde yeni bir 3.eleman oluşmaz. 
+
+            //isimler = new string[4];
+            //isimler[3] = "abcd";
+            //Console.WriteLine(isimler[3]);
+            //Console.WriteLine(isimler[0]); // değerleri kaybederiz. 0.eleman boş gelir. ancak List ile yaparsak;
+
+            List<string> isimler2 = new List<string> { "Rumeysa", "Sena", "Özdemir" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            isimler2.Add("abcd");
+            Console.WriteLine(isimler2[3]);
+            Console.WriteLine(isimler2[0]); // koleksiyonlarda arraylar gibi değerleri kaybetmizi engelleyecek alt yapı
+
+        }
+    }
+}
