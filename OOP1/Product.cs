c1a6c3eb2a49cd9cb8111af305d5739d38e9b963
Rumeysa Sena ÖZDEMİR <ozrumeysasena@gmail.com>
2023-09-19 18:42:16 +0300

    19 Eylül Ders Sonu

---
+++ OOP1/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; } // hangi katogorideyse onun Idsi
+        public string ProductName { get; set; } // ürün adı
+        public double UnitPrice { get; set; }  //  ürünün birim fiyatı
+        public int UnitInStock { get; set; }  //   ürün stok adedi
+
+        
+
+    }
+}
