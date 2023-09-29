aaa4891c15dcdc83bde839a694acaa042d554abd
Rumeysa Sena ÖZDEMİR <ozrumeysasena@gmail.com>
2023-09-29 14:04:16 +0300

    ..

---
+++ GameProject/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GameProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.Add(new Gamer 
+         
+            { 
+                Id = 1, 
+                BirthYear = 1994,
+                FirstName = "SENA", 
+                LastName = "ÖZDEMİR", 
+                IdentityNumber = 12345
+            }
+           );
+           
+        }
+    }
+}
---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    class UserValidationManager : IUserValidationService
+    {
+
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.BirthYear == 1994 && gamer.FirstName == "SENA" && gamer.LastName=="ÖZDEMİR" && gamer.IdentityNumber==12345)
+            {
+                return true;
+            }
+            else
+            {
+                return false; 
+            }
+        }
+    }
+}
